0f03a82f6d294cab58fc433c1036419542e348f8
jingwhale <jingwhale@yeah.net>
2019-05-16 08:21:16 +0800

    @update path

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 const path = require('path');
 const makelink = require('./makelink');
 
-
-const pathName = path.resolve(__dirname, '../..');
-console.log(pathName)
+const pathName = path.resolve('./');//当前命令运行的目录；__dirname：返回运行文件所在的目录
+console.log(pathName);
 
 const srcFile = pathName + '/README.md';
 
-console.log(srcFile)
+console.log(srcFile);
 
 makelink(srcFile,pathName);
